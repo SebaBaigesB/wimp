3f3ea11fcb4849dab969b4648ec6599fcb6fc1fd
Marion Phare <marion.phare@wanadoo.fr>
2019-08-21 17:01:42 +0200

    seeds & cards

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,15 @@ puts "Creating restaurants"
   r.save
   # desc = Faker::Restaurant.description
 end
+puts "Creating meals"
+5.times do
+  starter = Faker::Food.dish
+  main_course = Faker::Food.vegetables
+  dessert = Faker::Food.fruits
+  s = Meal.new( name: starter, restaurant_id: 1)
+  mc = Meal.new( name: main_course, restaurant_id: 1)
+  d = Meal.new( name: dessert, restaurant_id: 1)
+  s.save
+  mc.save
+  d.save
+end
