eed88e9cdb637d896d833a9fcdc195f37f4e20d2
Marion Phare <marion.phare@wanadoo.fr>
2019-08-22 15:07:12 +0200

    dnone

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,3 +4,14 @@ import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the styleshe
 import { initMapbox } from '../plugins/init_mapbox';
 
 initMapbox();
+
+
+
+
+const cards = document.querySelectorAll("cards");
+const plats = document.getElementById("fa-plus");
+
+
+plats.addEventListener("click", (event) => {
+  event.currentTarget.classList.toggle("d-none");
+});
