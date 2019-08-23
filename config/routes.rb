b80d89bbca4ab5635be1a7c927d6677bf00469fd
Xavier Maisonnat <x.maisonnat@gmail.com>
2019-08-23 16:41:59 +0200

    corrected namespaced routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
       end
     end
     resources :tags, only: [:index]
-    namespace :owner do
+    namespace :owners do
       resources :restaurants
       resources :meals
     end
