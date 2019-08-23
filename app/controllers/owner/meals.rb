5edf3185f02c5488920dbdabc46aa69964329dc6
sebastien baiges-bello <baigesbellosebastien@gmail.com>
2019-08-23 13:24:46 +0200

    50% api, owner meal 40%

--- app/controllers/owner/meals.rb
+++ app/controllers/owner/meals.rb
@@ -6,9 +6,18 @@ class Owner::MealsController < ApplicationController
   end
 
   def new
+    @current_user = Restaurant.where(user: current_user)
+    @meal = Meal.new
   end
 
   def create
+    @meal = Meal.new(meal_params)
+    @meal.user = current_user
+    if @meal.save
+      redirect_to meal_path(@meal)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -18,5 +27,15 @@ class Owner::MealsController < ApplicationController
   end
 
   def destroy
+    @current_user = Meal.where(user: current_user)
+    @meal = Meal.find(params[:id])
+    @meal.destroy
+    redirect_to meals_path
+  end
+
+  private
+
+  def meal_params
+    params.require(:meal).permit(:name, :description, :photo, :prix, :tags)
   end
 end
