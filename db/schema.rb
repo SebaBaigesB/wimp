52eae2b7f291817376851ccad2c38e268ef04ba9
sebastien baiges-bello <baigesbellosebastien@gmail.com>
2019-08-26 16:00:58 +0200

    pulled master

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_08_26_133225) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,8 +54,7 @@ ActiveRecord::Schema.define(version: 2019_08_26_133225) do
 
   create_table "reviews", force: :cascade do |t|
     t.text "content"
-    t.bigint "restaurant_id"
-    t.bigint "user_id"
+    t.integer "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -87,7 +84,6 @@ ActiveRecord::Schema.define(version: 2019_08_26_133225) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
