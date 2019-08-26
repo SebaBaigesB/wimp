b4babeb0ae4a201b09ed4acadec723fe068ce970
sebastien baiges-bello <baigesbellosebastien@gmail.com>
2019-08-26 16:44:01 +0200

    clean

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_08_26_133225) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,8 +54,7 @@ ActiveRecord::Schema.define(version: 2019_08_26_133225) do
 
   create_table "reviews", force: :cascade do |t|
     t.text "content"
-    t.bigint "restaurant_id"
-    t.bigint "user_id"
+    t.integer "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
