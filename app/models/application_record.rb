61a3dbaff3b0aa47ecb1c272b1039a7c32adada5
sebastien baiges-bello <baigesbellosebastien@gmail.com>
2019-08-29 18:35:42 +0200

    put flags on nav and continued yml

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,4 +2,19 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
   LOCALES = [:en, :fr, :es, :de, :it, 'zh-CN'.to_sym, :ja, :pt, :ko, :el, :ru, :th]
+
+  FLAGS = {
+   en: "united-kingdom.png",
+   fr: "france.png",
+   de: "germany.png",
+   it: "italy.png",
+   ja: "japan.png",
+   pt: "portugal.png",
+   ko: "south-korea.png",
+   es: "spain.png",
+   "zh-CN".to_sym => "china.png",
+   el: "greece.png",
+   th: "thailand.png",
+   ru: "russia.png"
+  }
 end
