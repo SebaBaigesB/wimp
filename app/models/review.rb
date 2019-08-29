fdd3a1e5d6df45bfc3702e985bf28bf988a17c9a
sebastien baiges-bello <baigesbellosebastien@gmail.com>
2019-08-29 09:34:17 +0200

    adding rev

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,14 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
   belongs_to :user
+   after_create :translates_with_api
+
+  def translates_with_api
+    translator = TranslateApiService.new
+    Review::LOCALES.select { |l| l != I18n.locale }.each do |l|
+      translate_content = translator.call(text: self.content, local: I18n.locale, target: l)
+      self.attributes = { content: translate_content, locale: l }
+      self.save
+    end
+  end
 end
