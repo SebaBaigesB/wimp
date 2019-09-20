f4caabae97800258287bdffe65ba555ad06c4321
Xavier Maisonnat <x.maisonnat@gmail.com>
2019-09-20 15:43:40 +0200

    Made the marker's info window redirect to the restaurant show

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -30,13 +30,6 @@ const addMarkersToMap = (map, markers) => {
   });
 };
 
-// const fitMapToMarkers = (map, markers) => {
-//   console.log(markers)
-//   const bounds = new mapboxgl.LngLatBounds();
-//   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-//   map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
-// };
-
 const fitMapToLocation = (map, geoLoc) => {
   navigator.geolocation.getCurrentPosition((data) => {
         const lat = data.coords.latitude;
@@ -63,7 +56,5 @@ const initMapbox = () => {
     });
   }
 };
-
-
 
 export { initMapbox };
